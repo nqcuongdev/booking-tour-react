ecb7241ff7101b20527b12afca7ecd2d912cee16
Kevin <nvngoc249@gmail.com>
2020-10-15 20:31:37 +0700

    add project

---
+++ front-end/src/components/TopHeader/TopHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container} from 'reactstrap';
+import './TopHeader.scss';
+
+const TopHeader = () => {
+    return (
+        <div className="top_header">
+            <Container></Container>
+        </div>
+    )
+}
+
+export default TopHeader;
