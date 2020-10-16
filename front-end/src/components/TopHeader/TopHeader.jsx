c078ffd9ac6181d5540cd5530ea5186ff95e4d94
Kevin <nvngoc249@gmail.com>
2020-10-16 11:17:58 +0700

    add code 16/10

--- front-end/src/components/TopHeader/TopHeader.jsx
+++ front-end/src/components/TopHeader/TopHeader.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import {Container} from 'reactstrap';
+import { FaPhoneAlt,FaMapMarkerAlt } from 'react-icons/fa';
 import './TopHeader.scss';
 
 const TopHeader = () => {
     return (
-        <div className="top_header">
+        <div className="top__header">
             <Container>
-                <div className="top_header content">
-                    <div className="top_header left">
-                        <a href="">Wellcome to booking !</a>
+                <div className="top__content">
+                    <div className="top__left">
+                        <a href="">Welcome to booking !</a>
+                    </div>
+                    <div className="top__right">
+                        <ul className="top__items">
+                            <li className="top__item">
+                                <FaPhoneAlt size={18} /> <a href="tel:0337317788" style={{ color: 'black' }}>0337317788</a>
+                            </li>
+                            <li className="top__item">
+                                <FaMapMarkerAlt size={18} /> <a href="">Nam Ky Khoi Nghia, Da Nang</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </Container>
