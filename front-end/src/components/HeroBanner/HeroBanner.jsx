986891abfad43524a227a61e31c347217395901a
Kevin <nvngoc249@gmail.com>
2020-10-21 20:42:52 +0700

    Update HeroBanner.jsx

--- front-end/src/components/HeroBanner/HeroBanner.jsx
+++ front-end/src/components/HeroBanner/HeroBanner.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { Container, Row } from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 import "./HeroBanner.scss";
+import banner from "../../assets/images/japan.jpg";
 
 const HeroBanner = () => {
   return (
-    <div>
+    <div style={{ backgroundImage: `url(${banner})` }}>
       <Container>
         <Row>
-          <div className="row justify-content-between align-items-center"></div>
+          <Col md={12} style={{ minHeight: "439px" }}>
+            <div className="hero__caption text-center">
+              <h4>Everyday</h4>
+              <h4>a new adventure</h4>
+            </div>
+            <div className="hero__form-search"></div>
+          </Col>
         </Row>
       </Container>
     </div>
