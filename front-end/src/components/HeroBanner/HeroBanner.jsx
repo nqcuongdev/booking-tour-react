6e082014a12ebd8b703abc2815b0b4b113d71f81
Kevin <nvngoc249@gmail.com>
2020-10-23 15:55:37 +0700

    Coding HeroBanner 23/10

--- front-end/src/components/HeroBanner/HeroBanner.jsx
+++ front-end/src/components/HeroBanner/HeroBanner.jsx
@@ -1,9 +1,34 @@
-import React from "react";
-import { Col, Container, Row } from "reactstrap";
+import React, { useState } from "react";
+import {
+  Col,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Nav,
+  NavItem,
+  NavLink,
+  Row,
+  TabContent,
+  TabPane,
+} from "reactstrap";
 import "./HeroBanner.scss";
+import { FaClock, FaMapMarkedAlt, FaMapMarker } from "react-icons/fa";
 import banner from "../../assets/images/japan.jpg";
+import "react-dates/initialize";
+import { DateRangePicker } from "react-dates";
+import moment from "moment";
 
 const HeroBanner = () => {
+  const [activateTab, setActiveTab] = useState("hotels");
+  const [dateRange, setDateRange] = useState({
+    startDate: moment().format("D/M/YYYY"),
+    endDate: moment().format("D/M/YYYY"),
+  });
+
+  const toggle = (tab) => {
+    if (activateTab !== tab) setActiveTab(tab);
+  };
   return (
     <div style={{ backgroundImage: `url(${banner})` }}>
       <Container>
@@ -13,7 +38,103 @@ const HeroBanner = () => {
               <h4>Everyday</h4>
               <h4>a new adventure</h4>
             </div>
-            <div className="hero__form-search"></div>
+            <div className="hero__form-search">
+              <Nav tabs>
+                <NavItem>
+                  <NavLink
+                    className={activateTab === "hotels" ? "active" : ""}
+                    onClick={() => {
+                      toggle("hotels");
+                    }}
+                  >
+                    Hotels
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    className={activateTab === "tours" ? "active" : ""}
+                    onClick={() => {
+                      toggle("tours");
+                    }}
+                  >
+                    Tours
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    className={activateTab === "events" ? "active" : ""}
+                    onClick={() => {
+                      toggle("events");
+                    }}
+                  >
+                    Events
+                  </NavLink>
+                </NavItem>
+              </Nav>
+              <TabContent activeTab={activateTab}>
+                <TabPane tabId="hotels">
+                  <Form className="hotels__form">
+                    <div className="field__search">
+                      <Row>
+                        <Col md={4} className="border-right">
+                          <FormGroup>
+                            <FaMapMarkedAlt size={18} />
+                            <div className="form__content">
+                              <label htmlFor="selectForm">Hotels</label>
+                              <Input
+                                type="select"
+                                name="hotels"
+                                id="selectForm"
+                              >
+                                <option>Where are you going ?</option>
+                                <option value="1">
+                                  <FaMapMarker size={18} /> 1
+                                </option>
+                                <option value="1">
+                                  <FaMapMarker size={18} /> 1
+                                </option>
+                                <option value="1">
+                                  <FaMapMarker size={18} /> 1
+                                </option>
+                                <option value="1">
+                                  <FaMapMarker size={18} /> 1
+                                </option>
+                              </Input>
+                            </div>
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                    </div>
+                  </Form>
+                </TabPane>
+                <TabPane tabId="tours">
+                  <Form>
+                    <div className="field__content">
+                      <Row>
+                        <Col md={4} className="border-right">
+                          <FormGroup>
+                            <FaClock size={18} />
+                            <div className="form__content">
+                              <div className="form__date__search">
+                                <DateRangePicker
+                                  startDate={dateRange.startDate}
+                                  endDate={dateRange.endDate}
+                                />
+                              </div>
+                            </div>
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                    </div>
+                  </Form>
+                </TabPane>
+                <TabPane tabId="events">
+                  <Form>
+                    <h1>Tab 3</h1>
+                  </Form>
+                </TabPane>
+              </TabContent>
+            </div>
           </Col>
         </Row>
       </Container>
