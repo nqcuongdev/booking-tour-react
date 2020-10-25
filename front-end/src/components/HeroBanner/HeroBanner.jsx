16052a1353a355c2c4b2650b8f9f2e0539080fbe
Kevin <nvngoc249@gmail.com>
2020-10-25 17:47:15 +0700

    css hero banner 25/10

--- front-end/src/components/HeroBanner/HeroBanner.jsx
+++ front-end/src/components/HeroBanner/HeroBanner.jsx
@@ -13,22 +13,17 @@ import {
   TabPane,
 } from "reactstrap";
 import "./HeroBanner.scss";
-import { FaClock, FaMapMarkedAlt, FaMapMarker } from "react-icons/fa";
 import banner from "../../assets/images/japan.jpg";
-import "react-dates/initialize";
-import "react-date-range/dist/styles.css";
-import "react-date-range/dist/theme/default.css";
+import Button from "reactstrap/lib/Button";
+import { FaCalendarCheck, FaHotel, FaTicketAlt } from "react-icons/fa";
 
 const HeroBanner = () => {
+  //Get current date and next date to fill in label
+  const today = new Date();
+  const tomorrow = new Date(today + 1);
+  tomorrow.setDate(today.getDate() + 1);
+
   const [activateTab, setActiveTab] = useState("hotels");
-  const [dateRange, setDateRange] = useState([
-    {
-      startDate: new Date(),
-      endDate: new Date() + 1,
-      key: "check-in-out",
-    },
-  ]);
-  const [showDateRange, setShowDateRange] = useState(false);
 
   const toggle = (tab) => {
     if (activateTab !== tab) setActiveTab(tab);
@@ -40,7 +35,7 @@ const HeroBanner = () => {
         <Row>
           <Col md={12} style={{ minHeight: "439px" }}>
             <div className="hero__caption text-center">
-              <h4>Everyday</h4>
+              <h1>Everyday</h1>
               <h4>a new adventure</h4>
             </div>
             <div className="hero__form-search">
@@ -52,6 +47,7 @@ const HeroBanner = () => {
                       toggle("hotels");
                     }}
                   >
+                    <FaHotel size={18} className="mr-2" />
                     Hotels
                   </NavLink>
                 </NavItem>
@@ -62,6 +58,7 @@ const HeroBanner = () => {
                       toggle("tours");
                     }}
                   >
+                    <FaTicketAlt size={18} className="mr-2" />
                     Tours
                   </NavLink>
                 </NavItem>
@@ -72,51 +69,50 @@ const HeroBanner = () => {
                       toggle("events");
                     }}
                   >
+                    <FaCalendarCheck size={18} className="mr-2" />
                     Events
                   </NavLink>
                 </NavItem>
               </Nav>
               <TabContent activeTab={activateTab}>
                 <TabPane tabId="hotels">
-                  <Form className="hotels__form">
+                  <Form className="main__form">
                     <div className="field__search">
                       <Row>
-                        <Col md={4} className="border-right">
-                          <FormGroup>
-                            <FaMapMarkedAlt size={18} />
-                            <div className="form__content">
-                              <label htmlFor="selectForm">Hotels</label>
-                              <Input
-                                type="select"
-                                name="hotels"
-                                id="selectForm"
-                              >
-                                <option>Where are you going ?</option>
-                                <option value="1">
-                                  <FaMapMarker size={18} /> 1
-                                </option>
-                                <option value="1">
-                                  <FaMapMarker size={18} /> 1
-                                </option>
-                                <option value="1">
-                                  <FaMapMarker size={18} /> 1
-                                </option>
-                                <option value="1">
-                                  <FaMapMarker size={18} /> 1
-                                </option>
-                              </Input>
-                            </div>
-                          </FormGroup>
-                        </Col>
-                        <Col md={4} className="border-right">
-                          <FormGroup>
-                            <FaClock size={18} />
-                            <div className="form__content">
-                              <label htmlFor="selectDate">Check In-Out</label>
-                              <div className="form__date-search"></div>
-                            </div>
-                          </FormGroup>
-                        </Col>
+                        <FormGroup className="col-sm-6 col-md-3">
+                          <label htmlFor="selectForm">Hotels</label>
+                          <Input type="select" name="hotels" id="selectForm">
+                            <option>Where ?</option>
+                            <option value="1">Vietnam</option>
+                          </Input>
+                        </FormGroup>
+                        <FormGroup className="col-sm-6 col-md-5">
+                          <Row>
+                            <Col xs={6}>
+                              <label htmlFor="selectDate">Check In</label>
+                              <Input type="date" />
+                            </Col>
+                            <Col xs={6}>
+                              <label htmlFor="selectDate">Check Out</label>
+                              <Input type="date" />
+                            </Col>
+                          </Row>
+                        </FormGroup>
+                        <FormGroup className="col-sm-6 col-md-2">
+                          <Row>
+                            <Col xs={6}>
+                              <label htmlFor="selectDate">Adult</label>
+                              <Input type="number" value="1" />
+                            </Col>
+                            <Col xs={6}>
+                              <label htmlFor="selectDate">Children</label>
+                              <Input type="number" value="1" />
+                            </Col>
+                          </Row>
+                        </FormGroup>
+                        <FormGroup className="col-sm-6 col-md-2 fix__height">
+                          <Button color="orange">SEARCH</Button>
+                        </FormGroup>
                       </Row>
                     </div>
                   </Form>
