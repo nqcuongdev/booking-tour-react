c06c13f8dcb62f86babeea820249bf8aab5e31da
nqcuong <nqcuong.dev@gmail.com>
2020-11-17 09:41:38 +0700

    Edit layout

--- front-end/src/layouts/MainLayout.jsx
+++ front-end/src/layouts/MainLayout.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
-import TopHeader from "../components/TopHeader/TopHeader";
 
 const MainLayout = (props) => {
   return (
     <React.Fragment>
-      <TopHeader />
       <Header />
       {props.children}
       <Footer />
