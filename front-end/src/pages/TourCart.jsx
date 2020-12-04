f9230e96f785bc2a90f038c0dee1b912593fb15b
nvchau <nvchau.dev@gmail.com>
2020-12-05 01:08:02 +0700

    Finish Cart-Tour

--- front-end/src/pages/TourCart.jsx
+++ front-end/src/pages/TourCart.jsx
@@ -62,77 +62,79 @@ const TourCart = props => {
 
                 <Container className="cart-content mt-50 mb-50">
                     <Form>
-                        <div class="scroll-cart">
-                            <div className="cart-col">
-                                <Row className="cart-col">
-                                    <Col xl={6} lg={6}>
-                                        <span>Product</span>
-                                    </Col>
-                                    <Col xl={2} lg={2}>
-                                        <span>Adult</span>
-                                    </Col>
-                                    <Col xl={2} lg={2}>
-                                        <span>Children</span>
-                                    </Col>
-                                    <Col xl={2} lg={2}>
-                                        <span>Total</span>
-                                    </Col>
-                                </Row>
-                            </div>
-                            <div className="cart-list">
-                                {cartData.map((item) => {
-                                    return (
-                                        <Row className="cart-item">
-                                            <Col xl={12} className="cart-title">{item.title}</Col>
-                                            <div className="cart-content">
-                                                <Col xl={6} lg={6} className="image-info">
-                                                    <Row>
-                                                        <Col xl={6} lg={6}
-                                                            className="image" 
-                                                            // style={{ backgroundImage: `url(${item.image})`}}
-                                                        >
-                                                            <img src={item.image} alt=""/>
-                                                        </Col>
-                                                        <Col xl={6} lg={6}>
-                                                            <p>Check in date: {item.checkInDate}</p>
-                                                            <p>Check out date: {item.checkOutDate}</p>
-                                                            <p>Adult: {item.adult}</p>
-                                                            <p>Children: {item.children}</p>
-                                                        </Col>
-                                                    </Row>
-                                                </Col>
-                                                <Col xl={2} className="adult">
-                                                    <span>
-                                                        <Input type="number" value={item.adult} />
-                                                        <p style={{ color: '#a8a7a7'}}>x ${item.priceOfAdult}</p>
-                                                    </span>
-                                                </Col>
-                                                <Col xl={2} className="children">
-                                                    <span>
-                                                        <Input type="number" value={item.children} />
-                                                        <p style={{ color: '#a8a7a7'}}>x ${item.priceOfChildren}</p>
-                                                    </span>
-                                                </Col>
-                                                <Col xl={2} className="total">
-                                                    ${calculationTotalItem(
-                                                        item.adult, item.children, item.priceOfAdult, item.priceOfChildren
-                                                    )}
-                                                    <div className="btn-delete">
-                                                        <Link><MdClose className="icon" /></Link>
-                                                    </div>
-                                                </Col>
-                                            </div>
-                                        </Row>
-                                    )
-                                })}
-                            </div>
+                        <div className="cart-col">
+                            <Row>
+                                <Col xl={6} lg={6}>
+                                    <span>Product</span>
+                                </Col>
+                                <Col xl={2} lg={2}>
+                                    <span>Adult</span>
+                                </Col>
+                                <Col xl={2} lg={2}>
+                                    <span>Children</span>
+                                </Col>
+                                <Col xl={2} lg={2}>
+                                    <span>Total</span>
+                                </Col>
+                            </Row>
                         </div>
+
+                        <div className="cart-list">
+                            {cartData.map((item) => {
+                                return (
+                                    <div className="cart-item">
+                                        <Row xl={12} lg={12} md={12} xs={12} className="cart-title">{item.title}</Row>
+                                        <Row className="cart-content">
+                                            <Col xl={3} lg={3} md={6} xs={6} className="image">
+                                                <img src={item.image} alt=""/>
+                                            </Col>
+                                            <Col xl={3} lg={3} md={6} xs={6} className="info">
+                                                <div>
+                                                    <p>Check in date: {item.checkInDate}</p>
+                                                    <p>Check out date: {item.checkOutDate}</p>
+                                                    <p>Adult: {item.adult}</p>
+                                                    <p>Children: {item.children}</p>
+                                                </div>
+                                            </Col>
+                                            <Col xl={2} lg={2} md={4} xs={4} className="adult">
+                                                <div>
+                                                    <span className="show-hide-scale">Adult</span>
+                                                    <Input type="number" value={item.adult} />
+                                                    <p>x ${item.priceOfAdult}</p>
+                                                </div>
+                                            </Col>
+                                            <Col xl={2} lg={2} md={4} xs={4} className="children">
+                                                <div>
+                                                    <span className="show-hide-scale">Children</span>
+                                                    <Input type="number" value={item.children} />
+                                                    <p>x ${item.priceOfChildren}</p>
+                                                </div>
+                                            </Col>
+                                            <Col xl={2} lg={2} md={4} xs={4} className="total">
+                                                <div className="price">
+                                                    <span className="show-hide-scale">Total</span>
+                                                    <span>
+                                                        $ {calculationTotalItem(
+                                                            item.adult, item.children, item.priceOfAdult, item.priceOfChildren
+                                                        )}
+                                                    </span>
+                                                </div>
+                                                <div className="btn-delete">
+                                                    <Link><MdClose className="icon" /></Link>
+                                                </div>
+                                            </Col>
+                                        </Row>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        
                         <Row className="total-pay">
                             <Col xl={6} lg={6} md={6} xs={12} className="input-gift">
                                 <Input placeholder="Enter your coupon code"/><FaGift className="icon"/>
                             </Col>
                             <Col xl={6} lg={6} md={6} xs={12} className="total-to-pay">
-                                <p>Total: <span>$2391</span></p>
+                                <p>Total: <span>$ 2391</span></p>
                             </Col>
                         </Row>
                         <hr className="mt-50 mb-50" />
