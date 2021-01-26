4999373a195e25a2d442f066210fcd07e8316ee2
nqcuong <nqcuong.dev@gmail.com>
2021-01-26 04:23:00 +0000

    Edit front sv

--- admin/src/helpers/api.js
+++ admin/src/helpers/api.js
@@ -4,19 +4,19 @@
  * @param {*} options
  */
 const fetchJSON = (url, options = {}) => {
-    return fetch(`http://localhost:6969/api/v1/${url}`, options)
-        .then((response) => {
-            if (!response.status === 200) {
-                throw response.json();
-            }
-            return response.json();
-        })
-        .then((json) => {
-            return json;
-        })
-        .catch((error) => {
-            throw error;
-        });
+  return fetch(`http://149.28.143.152:6969/api/v1/${url}`, options)
+    .then((response) => {
+      if (!response.status === 200) {
+        throw response.json();
+      }
+      return response.json();
+    })
+    .then((json) => {
+      return json;
+    })
+    .catch((error) => {
+      throw error;
+    });
 };
 
 export { fetchJSON };
--- admin/src/helpers/url.js
+++ admin/src/helpers/url.js
@@ -1,1 +1,1 @@
-export const url = 'http://localhost:6969';
+export const url = "http://149.28.143.152/:6969";
